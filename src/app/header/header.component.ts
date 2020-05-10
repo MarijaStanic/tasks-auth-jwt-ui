b49791e8f6a96f9d00106fcdfc3dd685fc943138
Marija Stanic <marija.stanic@hotmail.ca>
2020-05-10 17:31:15 +0100

    Enable menu links based on user authentication token

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BasicAuthenticationService } from '../service/basic-authentication.service';
 
 @Component({
   selector: 'app-header',
@@ -6,8 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  isUserLoggedIn: boolean = false;
 
-  constructor() { }
+  constructor(public authService: BasicAuthenticationService) { }
 
   ngOnInit(): void {
   }
