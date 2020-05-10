defb74c0f1b98f2dee2e195769687b452b9f5eab
Marija Stanic <marija.stanic@hotmail.ca>
2020-05-10 15:46:16 +0100

    Implement login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,25 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { BasicAuthenticationService } from '../service/basic-authentication.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  username: String;
+  password: String;
+  invalidLogin: boolean = false;
+  errorMessage: String;
 
-  username;
-  password;
-  invalidLogin = false;
-  errorMessage;
+  constructor(
+    private router: Router,
+    private basicAuthService: BasicAuthenticationService
+  ) {}
 
-  constructor(private router: Router) { }
-
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   handleLogin() {
-    this.router.navigate(['welcome', this.username]);
+    this.basicAuthService.authenticate(this.username, this.password).subscribe(
+      (data) => {
+        this.router.navigate(['welcome', this.username]);
+        this.invalidLogin = false;
+      },
+      (error) => {
+        this.invalidLogin = true;
+      }
+    );
   }
-
 }
--- src/app/service/basic-authentication.service.ts
+++ src/app/service/basic-authentication.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -13,13 +14,19 @@ export class BasicAuthenticationService {
     let headers = new HttpHeaders({
       Authorization: basicAuthHeaderString,
     });
-    return this.http.get<AuthenticationBean>(
-      `http://localhost:8080/basicAuth`,
-      {
-        headers,
-      }
-    );
+    return this.http
+      .get<AuthenticationBean>(`http://localhost:8080/basicauth`, {
+        headers
+      })
+      .pipe(
+        map((data) => {
+          sessionStorage.setItem('authenticatedUser', username);
+          return data;
+        })
+      );
   }
+
+  
 }
 
 export class AuthenticationBean {
